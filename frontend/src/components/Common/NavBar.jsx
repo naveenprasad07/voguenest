ec89dc307a07be56aee7968aef765eab30c27f44
Naveen Prasad <naveenacad89@gmail.com>
2025-02-03 23:21:39 +0530

    added hero footer explore new arrivals section

--- frontend/src/components/Common/NavBar.jsx
+++ frontend/src/components/Common/NavBar.jsx
@@ -7,13 +7,20 @@ import {
 import SearchBar from "./SearchBar";
 import CartDrawer from "../Layout/CartDrawer";
 import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
 
 const NavBar = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const [navDrawerOpen, setNavDrawerOpen] = useState(false);
+
+  const toggleNavDrawer = () => {
+    setNavDrawerOpen(!navDrawerOpen);
+  };
 
   const toggleCartDrawer = () => {
     setDrawerOpen(!drawerOpen);
   };
+
   return (
     <div>
       <nav className="container mx-auto flex items-center justify-between py-4 px-6">
@@ -66,12 +73,58 @@ const NavBar = () => {
           <div className="overflow-hidden">
             <SearchBar />
           </div>
-          <button className="md:hidden">
+          <button onClick={toggleNavDrawer} className="md:hidden">
             <HiBars3BottomRight className="h-6 w-6 text-gray-700" />
           </button>
         </div>
       </nav>
       <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer} />
+
+      {/* Mobile Navigation */}
+      <div
+        className={`fixed top-0 left-0 w-3/4 sm:w-1/2 md:2-1/3 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${
+          navDrawerOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <div className="flex justify-end p-5">
+          <button onClick={toggleNavDrawer}>
+            <IoMdClose className="h-6 w-6 text-gray-600 " />
+          </button>
+        </div>
+        <div className="p-4">
+          <h2 className="text-xl font-semibold mb-4">Menu</h2>
+          <nav className="space-y-4">
+            <Link
+              to="#"
+              onClick={toggleNavDrawer}
+              className="block text-gray-600 hover:text-black"
+            >
+              Men
+            </Link>
+            <Link
+              to="#"
+              onClick={toggleNavDrawer}
+              className="block text-gray-600 hover:text-black"
+            >
+              Women
+            </Link>
+            <Link
+              to="#"
+              onClick={toggleNavDrawer}
+              className="block text-gray-600 hover:text-black"
+            >
+              Top Wear
+            </Link>
+            <Link
+              to="#"
+              onClick={toggleNavDrawer}
+              className="block text-gray-600 hover:text-black"
+            >
+              Bottom Wear
+            </Link>
+          </nav>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/components/Layout/CartDrawer.jsx
+++ frontend/src/components/Layout/CartDrawer.jsx
@@ -1,9 +1,10 @@
 import { IoMdClose } from "react-icons/io";
+import CartContents from "../Cart/CartContents";
 
 const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
   return (
     <div
-      className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-1/4 h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${
+      className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${
         drawerOpen ? "translate-x-0 " : "translate-x-full"
       }`}
     >
@@ -16,14 +17,15 @@ const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
       {/* Cart contents with scrollable area*/}
       <div className="flex-grow p-4 overflow-y-auto">
         <h2 className="text-xl font-semibold mb-4">Your Cart</h2>
-        {/* Component for Car Contents  */}
+        {/* Component for Cart Contents  */}
+        <CartContents />
       </div>
       {/* Checkout button fixed at the bottom  */}
       <div className="p-4 bg-white sticky bottom-0">
-        <button className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition cursor-pointer ">
+        <button className="w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition cursor-pointer">
           Checkout
         </button>
-        <p className="text-sm tracking-tighter text-gray-500 mt-2 text-center ">
+        <p className="text-sm tracking-tighter text-gray-500 mt-2 text-center">
           Shipping,taxes, and discount codes calculated at checkout.
         </p>
       </div>

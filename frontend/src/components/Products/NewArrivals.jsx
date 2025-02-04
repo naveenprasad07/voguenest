e14ae846177910f4ffb1aaaf524af7301466163f
Naveen Prasad <naveenacad89@gmail.com>
2025-02-04 20:41:01 +0530

    add new arrivals

--- frontend/src/components/Products/NewArrivals.jsx
+++ frontend/src/components/Products/NewArrivals.jsx
@@ -100,6 +100,23 @@ const NewArrivals = () => {
     },
   ];
 
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
+    setStartX(e.pageX - scrollRef.current.offsetLeft);
+    setScrollLeft(scrollRef.current.scrollLeft);
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isDragging) return;
+    const x = e.pageX - scrollRef.current.offsetLeft;
+    const walk = x - startX;
+    scrollRef.current.scrollLeft = scrollLeft - walk;
+  };
+
+  const handleMouseUpOrLeave = () => {
+    setIsDragging(false);
+  };
+
   const scroll = (direction) => {
     const scrollAmount = direction === "left" ? -300 : 300;
     scrollRef.current.scrollBy({ left: scrollAmount, behaviour: "smooth" });
@@ -118,6 +135,7 @@ const NewArrivals = () => {
       scrollLeft: container.scrollLeft,
       clientWidth: container.clientWidth,
       containerScrollWidth: container.scrollWidth,
+      offsetLeft: scrollRef.current.offsetLeft,
     });
   };
 
@@ -126,11 +144,12 @@ const NewArrivals = () => {
     if (container) {
       container.addEventListener("scroll", updateScrollButtons);
       updateScrollButtons();
+      return () => container.removeEventListener("scroll", updateScrollButtons);
     }
-  });
+  }, []);
 
   return (
-    <section>
+    <section className="py-16 px-4 lg:px-0">
       <div className="container mx-auto text-center mb-10 relative">
         <h2 className="text-3xl font-bold mb-4">Explore New Arrivals</h2>
         <p className="text-lg text-gray-600 mb-8">
@@ -166,11 +185,13 @@ const NewArrivals = () => {
       {/* Scrollable Content  */}
       <div
         ref={scrollRef}
-        className="container mx-auto overflow-x-scroll flex space-x-6 relative"
-        // onMouseDown={handleMouseDown}
-        // onMouseMove={handleMouseMove}
-        // onMouseUp={handleMouseUpOrLeave}
-        // onMouseLeave={handleMouseUpOrLeave}
+        className={`container mx-auto overflow-x-scroll flex space-x-6 relative ${
+          isDragging ? "cursor-grabbing" : "cursor-grab"
+        }`}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUpOrLeave}
+        onMouseLeave={handleMouseUpOrLeave}
       >
         {newArrivals.map((product) => (
           <div
@@ -181,6 +202,7 @@ const NewArrivals = () => {
               src={product.images[0]?.url}
               alt={product.images[0]?.altText || product.name}
               className="w-full h-[500[x] object-cover rounded-lg"
+              draggable="false"
             />
             <div className="absolute bottom-0 left-0 right-0  backdrop-blur-md text-white/50 p-4 rounded-b-lg">
               <Link to={`/product/${product._id}`} className="block">

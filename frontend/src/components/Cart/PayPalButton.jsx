7b7c951065593edbf20470c5793b431c32dff404
Naveen Prasad <naveenacad89@gmail.com>
2025-02-11 17:07:50 +0530

    add-paypal-button

---
+++ frontend/src/components/Cart/PayPalButton.jsx
@@ -0,0 +1,27 @@
+import { PayPalButtons, PayPalScriptProvider } from "@paypal/react-paypal-js";
+
+const PayPalButton = ({ amount, onSuccess, onError }) => {
+  return (
+    <PayPalScriptProvider
+      options={{
+        "client-id":
+          "AXTtLEckls3L_6_PMwFEOlfrXqFo6bk1Oxcgeqt1QUWG1hxhf2b0TeSer_5phevanZ1qxmViIVHyPLMo",
+      }}
+    >
+      <PayPalButtons
+        style={{ layout: "vertical" }}
+        createOrder={(data, actions) => {
+          return actions.order.create({
+            purchase_units: [{ amount: { value: amount } }],
+          });
+        }}
+        onApprove={(data, actions) => {
+          return actions.order.capture().then(onSuccess);
+        }}
+        onError={onError}
+      />
+    </PayPalScriptProvider>
+  );
+};
+
+export default PayPalButton;

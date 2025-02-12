deb629730b622a0a7b704473f255c0c2676c61fc
Naveen Prasad <naveenacad89@gmail.com>
2025-02-12 17:54:30 +0530

    add-order-details-page

--- frontend/src/pages/MyOrdersPage.jsx
+++ frontend/src/pages/MyOrdersPage.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const MyOrdersPage = () => {
   const [orders, setOrders] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     setTimeout(() => {
@@ -35,7 +37,11 @@ const MyOrdersPage = () => {
       ];
       setOrders(mockOrders);
     }, 1000);
-  });
+  }, []);
+
+  const handleRowClick = (orderId) => {
+    navigate(`/order/${orderId}`);
+  };
 
   return (
     <div className="max-w-7xl mx-auto p-4 sm:p-6">
@@ -58,6 +64,7 @@ const MyOrdersPage = () => {
               orders.map((order) => (
                 <tr
                   key={order._id}
+                  onClick={() => handleRowClick(order._id)}
                   className="border-b hover:border-gray-50 cursor-pointer"
                 >
                   <td className="py-2 px-2 sm:py-4 sm:px-4">

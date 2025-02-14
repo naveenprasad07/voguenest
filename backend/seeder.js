5404a83ca2746cb684f2a5cb48d19987a30853cf
Naveen Prasad <naveenacad89@gmail.com>
2025-02-14 22:19:28 +0530

    add-api-products

---
+++ backend/seeder.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import Product from "./models/Product.js";
+import User from "./models/User.js";
+import products from "./data/products.js";
+
+dotenv.config();
+
+// Connect to mongoDB
+mongoose.connect(process.env.MONGO_URI);
+
+// Function to seed data
+
+const seedData = async () => {
+  try {
+    // Clear existing data
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    // Create a default admin User
+    const createdUser = await User({
+      name: "Admin User",
+      email: "admin@example.com",
+      password: "123456",
+      role: "admin",
+    });
+
+    // Assign the default user ID to each product
+    const userID = createdUser._id;
+
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: userID };
+    });
+
+    // Insert the products into the database
+    await Product.insertMany(sampleProducts);
+    console.log("Product data seeded successfully");
+    process.exit();
+  } catch (err) {
+    console.error("Error seeding the data");
+    process.exit(1);
+  }
+};
+
+seedData();
